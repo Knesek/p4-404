21ceee56bb908525c285fcf1187f7c53900f4df3
Knesek <mknesek@eagles.ewu.edu>
2014-11-18 00:02:58 -0800

    Version1

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//		jQuery(function($) {
+//		  $('#sky').jQlouds({ 
+//		  skyHeight: 400, 
+//		  wind: true, 
+//		  maxWidth: 500,
+//		  maxHeight: 400, 
+//		  minClouds: 1,
+//		  maxClouds: 3,
+//		  src: 'img/angel.png' });
+//		});
