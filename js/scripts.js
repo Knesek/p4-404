c96a5e8df64ef5377ea5ddf2b8ea1a7563e1bfb9
Knesek <mknesek@eagles.ewu.edu>
2014-11-20 00:44:13 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,52 @@
-//		jQuery(function($) {
+$(".a").blinker({
+    timeHidden: 20,
+    intervalRangeStart: 150,
+    intervalRangeStop: 500
+});
+
+//jQuery(function($) {
 //		  $('#sky').jQlouds({ 
 //		  skyHeight: 400, 
 //		  wind: true, 
-//		  maxWidth: 500,
-//		  maxHeight: 400, 
-//		  minClouds: 1,
-//		  maxClouds: 3,
-//		  src: 'img/angel.png' });
+//		  minClouds: 3,
+//		  maxClouds: 10, });
 //		});
+//$('.hi').letterfx({"fx":"smear","letter_end":"rewind"});
+//$('.bg').shards([0,0,0,.5],[255,0,0,.2],[255,255,0,.1],10,2,2,.1,true);
+//$('.bg').shards([0,0,0,.5],[255,0,0,.2],[255,255,0,.1],10,2,2,.1,true);
+//var p = $("p");
+
+//p.blinker();
+
+//p.bind({
+//    mouseenter: function(){
+  //      $(this).data("blinker").pause();
+    //},
+    // resume blinking on mouseleave
+//    mouseleave: function(){
+  //      $(this).data("blinker").blinkagain();
+    //}
+//}); 
+
+//$('h1').airport([ 'Welcome to Night Vale', 'ALL HAIL THE GLOW CLOUD', 'hello? HELLO?!' ]);
+
+//console.log($('h1').airport());
+
+//$('h1').click(function(){
+//	$('h1').hide
+//});
+
+//jQuery(function($) { 
+//	$('.a').blinker({
+ //     duration: '300'
+ //   });
+//});
+//console.log($('h1').modernBlink());
+
+//$( ".a" ).click(function() {
+//  $( ".a" ).fadeIn( "slow", function() {
+    // Animation complete
+//  });
+//});
+//console.log($('.a').fadeIn());
+
